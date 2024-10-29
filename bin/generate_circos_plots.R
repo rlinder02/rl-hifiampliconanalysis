8a1e0183f3de60dbbe4a590278777196bca4c918
RL <rlinder@Chuns-Mac-mini.burnham.org>
2024-10-29 10:22:36 -0700

    TS circos

--- bin/generate_circos_plots.R
+++ bin/generate_circos_plots.R
@@ -150,13 +150,13 @@ lapply(vcf_list$V1[c(1:5)], function(vcf) {
   vcf_struct_df <- pre.process.vcf.structure(vcf)
   vcf_muts_df <- pre.process.vcf.mutations(vcf, ref_bed_dt)
   vcf_max_depth <- vcf.read.depth(vcf)
-  counter <<- counter + 1
-  circos.genomicTrack(vcf_struct_df, ylim = c(0, 1), track.height = 0.05, bg.border = NA, panel.fun = function(region, value, ...) {
-                        i = getI(...)
-                        xlim = CELL_META$xlim
-                        circos.rect(region$start, 0, region$end, 1, col = "white", border = "black", track.index = counter)
-  })
-  circos.genomicTrack(vcf_muts_df, numeric.column = 4, ylim = c(0, 1), track.height = 0.05, bg.border = NA, panel.fun = function(region, value, ...) {
+  # counter <<- counter + 1
+  # circos.genomicTrack(vcf_struct_df, ylim = c(0, 1), track.height = 0.05, bg.border = NA, panel.fun = function(region, value, ...) {
+  #                       i = getI(...)
+  #                       xlim = CELL_META$xlim
+  #                       circos.rect(region$start, 0, region$end, 1, col = "white", border = "black", track.index = counter)
+  # })
+  circos.genomicTrack(vcf_muts_df, numeric.column = 4, ylim = c(0, 1), track.height = 0.05 , panel.fun = function(region, value, ...) {
                         i = getI(...)
                         print(vcf_muts_df)
                         print(vcf_muts_df$symbol)
@@ -166,7 +166,7 @@ lapply(vcf_list$V1[c(1:5)], function(vcf) {
                         print(i)
                         print(ref_bed_dt)
                         xlim = CELL_META$xlim
-                        circos.genomicPoints(region, value, cex = 0.5, col = "red", track.index = counter)
+                        circos.genomicPoints(region, value, cex = 0.5, col = "red")
   })
 })
 circos.clear()
