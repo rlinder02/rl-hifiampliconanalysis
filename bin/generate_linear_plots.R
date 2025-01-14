85428a39344d4d0fdc6d128c2ed71836fa012a70
RL <rlinder@Mac.burnham.org>
2025-01-14 11:38:47 -0800

    TS transcript plot params

--- bin/generate_linear_plots.R
+++ bin/generate_linear_plots.R
@@ -474,9 +474,9 @@ struct_plot <- ggplot() +
   theme(aspect.ratio = (0.017544 + 0.081579*length(unique(id_orf_dfs$genc_id)))) +
   guides(fill = guide_legend(override.aes = list(shape = NA, border = NA)), colour = guide_legend(override.aes = list(size = 2)))
 
-my_legend <- get_legend(struct_plot + theme(legend.box.margin = margin(0, 0, 0, 12)))
+my_legend <- get_legend(struct_plot + theme(legend.box.margin = margin(0, 0, 0, 6)))
 
-struct_plot_nl <- plot_grid(struct_plot + theme(legend.position="none"))
+struct_plot_nl <- struct_plot + theme(legend.position="none")
 
 struct_plot_l <- plot_grid(struct_plot_nl, my_legend, rel_widths = c(4,1), align = 'vh', hjust = -1, nrow = 1)
 
